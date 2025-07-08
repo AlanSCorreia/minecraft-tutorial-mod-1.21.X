374b0650e527aed5cb6b3596c1da2ca405208b2b
AlanSCorreia <alancorreia110@gmail.com>
2025-07-08 17:23:07 -0300

    Added items: Raw Pink Garnet, Pink Garnet

---
+++ src/main/java/com/bababoi/tutorialmod/item/ModItems.java
@@ -0,0 +1,30 @@
+package com.bababoi.tutorialmod.item;
+
+import com.bababoi.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    // Items to be registered
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    // Helper method for register items
+    public static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    // Registering items
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for" + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+}
